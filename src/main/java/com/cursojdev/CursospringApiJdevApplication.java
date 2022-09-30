6b1ac446522f2fbf7f0d94bda2d6e7ffd6214580
will henrique <willhc1984@gmail.com>
2022-09-30 11:00:06 -0300

    Setup inicial IDE STS

---
+++ src/main/java/com/cursojdev/CursospringApiJdevApplication.java
@@ -0,0 +1,13 @@
+package com.cursojdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringApiJdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringApiJdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursojdev/CursospringApiJdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApiJdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
